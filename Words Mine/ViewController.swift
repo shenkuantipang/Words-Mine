8675f84e65fc2fe16e1b3234bff57b98e0a1c140
vivi00008 <70990464+vivi00008@users.noreply.github.com>
2021-05-18 16:22:53 +0700

    Initial Commit

---
+++ Words Mine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Words Mine
+//
+//  Created by Warit Karnbunjob on 18/5/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
